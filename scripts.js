51215661e4f94cd2bccb9ddc43d0211dc8689307
Gigantoherceg <mgaboor@gmail.com>
2018-08-08 20:56:45 +0200

    Add responsive design and some style.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
-var document = document.getElementById('myDocument');
+var document = document.getElementById("asd");
 
-document.write('<h1 align=center>Turbina-teljesítmények kiiratása és összegzése</h1>');
+document.getElementById("asd").innerHTML = "<h1 align=center>Turbina-teljesítmények kiiratása és összegzése</h1>";
 
 var allTurbins = 25;
 var sumPower = 0;
@@ -9,19 +9,19 @@ var fullPower = 2000;
 var halfPower = 1000;
 var noPower = 0;
 
-document.write('<br><h3>');
+document.getElementById("scrAns").innerHTML = "<br><h3>";
 
 for (turbineCounter = 1; turbineCounter <=allTurbins; turbineCounter++){
   if (turbineCounter <= 10){
     sumPower = sumPower + fullPower;
-    document.write('A(z) ' + turbineCounter + '. számú szélturbina ' + 'teljes' + ' fordulaton működik, ' + fullPower + 'MWh áramot termelve. A farm összteljesítménye jelenleg ' + sumPower + 'MWh.');
+    document.getElementById("scrAns").innerHTML = "A(z) " + turbineCounter + ". számú szélturbina " + "teljes" + " fordulaton működik, " + fullPower + "MWh áramot termelve. A farm összteljesítménye jelenleg " + sumPower + "MWh.";
   } else if (turbineCounter <= 20) {
     sumPower = sumPower + halfPower;
-    document.write('A(z) ' + turbineCounter + '. számú szélturbina ' + 'fél' + ' fordulaton működik, ' + halfPower + 'MWh áramot termelve. A farm összteljesítménye jelenleg ' + sumPower + 'MWh.');
+    document.getElementById("scrAns").innerHTML = "A(z) " + turbineCounter + ". számú szélturbina " + "fél" + " fordulaton működik, " + halfPower + "MWh áramot termelve. A farm összteljesítménye jelenleg " + sumPower + "MWh.";
   } else {
     sumPower = sumPower + noPower;
-    document.write('A(z) ' + turbineCounter + '. számú szélturbina ' + 'áll, ' + noPower + 'MWh áramot termelve. A farm összteljesítménye jelenleg ' + sumPower + 'MWh.');
+    document.getElementById("scrAns").innerHTML = "A(z) " + turbineCounter + ". számú szélturbina " + "áll, " + noPower + "MWh áramot termelve. A farm összteljesítménye jelenleg " + sumPower + "MWh.";
   }
-document.write('<br>');
+document.getElementById("scrAns").innerHTML = "<br>";
 }
-document.write('</h3>');
+document.getElementById("scrAns").innerHTML = "</h3>";
